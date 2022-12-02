306cdc8474da7d61143508d11975c4873980a65e
ToppSpin <jacobram@outlook.com>
2022-12-02 23:02:35 +0100

    Done!

--- client.py
+++ client.py
@@ -1,1 +1,11 @@
+import socket            
+s = socket.socket()        
+port = 6666
+
+s.connect(('78.71.14.72', port))
+
+while True:
+    x = input("")
+    s.send(x.encode())
+    print(s.recv(1024).decode())
 
--- server.py
+++ server.py
@@ -1,1 +1,14 @@
+import socket
+s = socket.socket()
+port = 6666
 
+s.bind(('', port))
+
+s.listen(5)
+c, addr = s.accept()
+print ('Got connection from', addr)
+
+while True:
+    print(c.recv(1024).decode())
+    x = input("")
+    c.send(x.encode())
