ce10f138f0e84ec9713c03d01ff93e52424c0733
JRameke <tishel20jacram@innovationsgymnasiet.se>
2022-12-09 18:31:39 +0100

    Yes!

--- SocketServer.py
+++ SocketServer.py
@@ -1,27 +1,21 @@
 import socketserver
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
+class MyUDPHandler(socketserver.BaseRequestHandler):
     """
-    The request handler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
+    This class works similar to the TCP handler class, except that
+    self.request consists of a pair of data and client socket, and since
+    there is no connection the client address must be given explicitly
+    when sending data back via sendto().
     """
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
+        data = self.request[0].strip()
+        socket = self.request[1]
         print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        print(data, "Hejdå")
+        socket.sendto(data, self.client_address)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
-
-    # Create the server, binding to localhost on port 9999
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        # Activate the server; this will keep running until you
-        # interrupt the program with Ctrl-C
+    with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
         server.serve_forever()
--- server.py
+++ server.py
@@ -1,25 +1,43 @@
-import socket
-import multiprocessing as mp
+#!/usr/bin/python           # This is server.py file                                                                                                                                                                           
 
-def con(Connection):
-    print("yoyo")
-    while True:
-        print(">> ", Connection.recv(1024).decode())
-        Connection.send("2".encode())
+import socket               # Import socket module
+import threading as th
 
+Threads = {}
 connections = {}
-
-s = socket.socket()
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
 port = 6666
 i = 0
 
-if __name__ == '__main__':
+def newClient(c, addr):
+    x = str(addr) + "din mamma"
+    sender(x, addr)
     while True:
-        s.bind(('', port))
-        s.listen(5)
-        c, addr = s.accept()
-        print ('Got connection from', addr)
-        connections.update({i:mp.Process(target=con, args=(c,))})
-        connections[i].start()
-        i += 1
-        port = port + 1
+        msg = c.recv(123456789).decode()
+        sender(msg, addr)
+        
+
+def sender(msg, addr): 
+    for i in connections.items():
+        if i[0] == addr:
+            continue
+        i[1].send(msg.encode())
+
+print('Server started!')
+print('Waiting for clients...')
+
+s.bind((host, port))        # Bind to the port
+s.listen(5)                 # Now wait for client connection.
+
+
+while True:
+    c, addr = s.accept()     # Establish connection with client.
+    print('Got connection from', addr)
+    connections.update({i: c})
+    Threads.update({i: th.Thread(target=newClient, args=(c, i,))})
+    Threads[i].start()
+    i =+ 1
+
+
+   
